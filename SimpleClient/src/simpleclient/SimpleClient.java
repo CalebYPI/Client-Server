bd2c00f69ed657910a23b5b94047acf96949b209
TyronneLloyd <61544299+TyronneLloyd@users.noreply.github.com>
2020-10-28 21:48:00 +0200

    Add files via upload

---
+++ SimpleClient/src/simpleclient/SimpleClient.java
@@ -0,0 +1,44 @@
+/* SimpleClient.java
+   This is the client program
+   Author: Kruben Naidoo
+   Date: 15 September 2020 */
+
+package simpleclient;
+
+import java.net.*;
+import java.io.*;
+
+public class SimpleClient {
+    public static void main(String args[]) {
+        try {
+            // Open your connection to a server, at port 5432
+            // localhost used here
+            Socket s1 = new Socket("localhost", 5432);
+            System.out.println("Connection established at port 5432");
+
+            // Get an input stream from the socket
+            InputStream is = s1.getInputStream();
+            // Decorate it with a "data" input stream
+            DataInputStream dis = new DataInputStream(is);
+            System.out.println("Getting data...");
+
+            // Read the input and print it to the screen
+            System.out.println(dis.readUTF());
+
+            // When done, just close the stream and connection
+            dis.close();
+            s1.close();
+            System.out.println("Connection closed.");
+        }
+        catch (ConnectException connExcep) {
+            System.out.println("Error: " + connExcep.getMessage());
+        }
+        catch (IOException ioExcep) {
+            System.out.println("Error: " + ioExcep.getMessage());    
+        }
+        catch (Exception e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+    }
+}
+			
